49a263693cf6ca885a68a0583ce6c08b76470a08
DianaAyala2 <00021325@uca.edu.sv>
2026-01-16 16:07:09 -0600

    add: nodos, lista enlazada y calculo de promedio

--- Lab2PED.cpp
+++ Lab2PED.cpp
@@ -2,34 +2,96 @@
 #include <string>
 using namespace std;
 
-class Estudiante 
+class Estudiante
 {
-    private:
+private:
     string nombre;
-    float notaFinal;
-    
-    public:
-        Estudiante(string n, float cal)
-        {
-            nombre = n;
-            notaFinal = cal;
-        }
+    float notafinal;
 
-        void imprimir()
+public:
+    Estudiante(string n, float notaf) : nombre(n), notafinal(notaf) {}
+    float nota()
+    {
+        return notafinal;
+    }
+    void imprimir()
+    {
+        cout << nombre << "su nota es: " << notafinal << endl;
+    }
+};
+
+struct nodo
+{
+    Estudiante *est;
+    nodo *sig;
+
+    nodo(Estudiante *e) : est(e), sig(nullptr) {}
+};
+
+class ListaEnlazada
+{
+private:
+    nodo *inicio;
+    float sumarNotas(nodo *n, int &cant) // para el total de la nota
+    {
+        if (n == nullptr)
+            return 0;
+        cant++;
+
+        return n->est->nota() + sumarNotas(n->sig, cant);
+    }
+
+public:
+    ListaEnlazada() : inicio(nullptr) {}
+
+    void agregarEstudiante(string nombre, float nota)
+    {
+        nodo *nuevoEstudiante = new nodo(new Estudiante(nombre, nota));
+
+        if (!inicio)
         {
-        cout << nombre << " - Nota: " << notaFinal << endl;
+            inicio = nuevoEstudiante;
         }
+        else
+        {
+            nodo *temp = inicio;
+            while (temp->sig)
+                temp = temp->sig;
+            temp->sig = nuevoEstudiante;
+        }
+    }
+    void ImprimirLista()
+    {
+        cout << "--- Estudiantes ---" << endl;
+        nodo *temp = inicio;
+        while (temp)
+        {
+            temp->est->imprimir();
+            temp = temp->sig;
+        }
+    }
+
+    float calcularPromedio()
+    {
+        int total = 0;
+        float suma = sumarNotas(inicio, total);
+        return total ? suma / total : 0;
+    }
 };
 
 
+int main()
+{
+    ListaEnlazada lista;
 
-class ListaEnlazada {
-    private:
+    lista.agregarEstudiante("Juan", 85.5f);
+    lista.agregarEstudiante("Maria", 92.0f);
+    lista.agregarEstudiante("Pedro", 78.5f);
+    lista.agregarEstudiante("Ana", 88.0f);
 
-    public:
-};
+    lista.ImprimirLista();
 
-int main(){
+    cout << "Promedio: " << lista.calcularPromedio() << endl;
 
     return 0;
-}
+};
