2268b6b27d624b51fa3e84536b45a088947ce047
JACR07 <00054525@uca.edu.sv>
2026-01-16 11:32:57 -0800

    add: main code

---
+++ Lab2PED.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Estudiante {
+    private:
+
+    public:
+};
+
+class ListaEnlazada {
+    private:
+
+    public:
+};
+
+int main(){
+    
+    return 0;
+}
