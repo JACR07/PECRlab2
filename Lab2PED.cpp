7e6f8e38eb3872dc8f1cb3ca2174561d4a9f6ab3
JACR07 <00054525@uca.edu.sv>
2026-01-16 12:21:36 -0800

    add: classestudiantehechaconsumetodo

--- Lab2PED.cpp
+++ Lab2PED.cpp
@@ -2,11 +2,26 @@
 #include <string>
 using namespace std;
 
-class Estudiante {
+class Estudiante 
+{
     private:
-
+    string nombre;
+    float notaFinal;
+    
     public:
+        Estudiante(string n, float cal)
+        {
+            nombre = n;
+            notaFinal = cal;
+        }
+
+        void imprimir()
+        {
+        cout << nombre << " - Nota: " << notaFinal << endl;
+        }
 };
+
+
 
 class ListaEnlazada {
     private:
@@ -15,6 +30,6 @@ class ListaEnlazada {
 };
 
 int main(){
-    
+
     return 0;
 }
